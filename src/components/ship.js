eb8ff45e76f9f1f3d04faa70704543b5ed2e9357
carisaelam <carisaelam@gmail.com>
2024-12-08 16:18:01 -0500

    Add hit and isSunk functions

--- src/components/ship.js
+++ src/components/ship.js
@@ -1,7 +1,19 @@
 export class Ship {
-  constructor(length, numberOfHits = 0, isSunk = false) {
+  constructor(length, numberOfHits = 0, sunkStatus = false) {
     this.length = length;
     this.numberOfHits = numberOfHits;
-    this.isSunk = isSunk;
+    this.sunkStatus = sunkStatus;
+  }
+
+  hit() {
+    this.numberOfHits = this.numberOfHits + 1;
+    return this.numberOfHits;
+  }
+
+  isSunk() {
+    if (this.numberOfHits >= this.length) {
+      this.sunkStatus = true;
+    }
+    return this.sunkStatus;
   }
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,11 +1,12 @@
 import { describe, it, expect } from 'vitest';
 import { Ship } from '../src/components/ship';
 
-describe('Ship Class', () => {
-  const ship = new Ship(1);
+const ship = new Ship(1);
 
-  console.log('ship: ', ship);
+console.log('ship: ', ship);
 
+// Constructor
+describe('Constructor', () => {
   it('should exist', () => {
     expect(ship).toBeTruthy;
   });
@@ -18,7 +19,28 @@ describe('Ship Class', () => {
     expect(ship.numberOfHits).toBe(0);
   });
 
-  it('should include isSunk', () => {
-    expect(ship.isSunk).toBe(false);
+  it('should include sunkStatus', () => {
+    expect(ship.sunkStatus).toBe(false);
+  });
+});
+
+// Hit function
+describe('Hit function', () => {
+  it('should increase numberOfHits by one', () => {
+    ship.hit();
+    expect(ship.numberOfHits).toEqual(1);
+  });
+});
+
+// isSunk function
+describe('Is Sunk function', () => {
+  it('should change sunkStatus to true if hits >= length', () => {
+    let twoShip = new Ship(2);
+    console.log('twoShip', twoShip);
+
+    twoShip.hit();
+    twoShip.hit();
+
+    expect(twoShip.isSunk()).toEqual(true);
   });
 });
