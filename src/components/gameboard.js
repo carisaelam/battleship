bfffa984634f430d73924ce397a85f2769e932b2
carisaelam <carisaelam@gmail.com>
2024-12-08 16:42:11 -0500

    Create getBoard and buildBuild functions

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -1,1 +1,20 @@
-export class Gameboard {}
+export class Gameboard {
+  constructor(size = 10) {
+    this.size = size;
+    this.board = this.buildBoard(this.size);
+  }
+
+  buildBoard(size) {
+    let board = [];
+
+    for (let row = 0; row < size; row++) {
+      board.push(Array(size).fill(null));
+    }
+    return board;
+  }
+
+  getBoard() {
+    console.log(this.board);
+    return this.board;
+  }
+}
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,10 +1,26 @@
 import { describe, it, expect } from 'vitest';
 import { Gameboard } from '../src/components/gameboard';
 
+// Constructor
 describe('Gameboard class', () => {
   const gameboard = new Gameboard();
-
   it('should exist', () => {
     expect(gameboard).toBeTruthy;
+  });
+
+  it('should have a size of 10', () => {
+    expect(gameboard.size).toBe(10);
+  });
+});
+
+// getBoard
+describe('getBoard function', () => {
+  const gameboard = new Gameboard(2);
+  
+  it('should return the gameboard', () => {
+    expect(gameboard.getBoard()).toEqual([
+      [null, null],
+      [null, null],
+    ]);
   });
 });
